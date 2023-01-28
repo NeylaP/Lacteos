3e19aa0b255dbadab760583d6f47271b5d0a13c1
Neyla Peña <neyla1998@outlook.es>
2023-01-28 08:23:04 -0500

    Ajustes en Models

--- Empresa_Lacteos/api/models.py
+++ Empresa_Lacteos/api/models.py
@@ -19,10 +19,12 @@ class Producto(models.Model):
     detalle=models.TextField(null=True, blank=True)
     stock=models.CharField(max_length=10)
 
+#En este modelo se relacionan el portafolio con los productos que pertenecen a este
 class Rel_Producto_Portafolio(models.Model):
     portafolio=models.ForeignKey('Portafolio', on_delete=models.PROTECT,  related_name='portafolio_prod', null=True)
     producto=models.ForeignKey('Producto', on_delete=models.PROTECT,  related_name='prod_portafolio', null=True)
 
+#En este modelo se relacionan el vendedor con los establecimientos que tiene a cargo
 class Rel_Vendedor_Establecimiento(models.Model):
     Vendedor=models.ForeignKey('Vendedor', on_delete=models.PROTECT,  related_name='vendedor_est', null=True)
     Establecimiento=models.ForeignKey('Establecimiento', on_delete=models.PROTECT,  related_name='establecimiento_vend', null=True)
